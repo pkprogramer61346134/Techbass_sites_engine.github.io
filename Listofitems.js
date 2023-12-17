36be34c98ed09869674cb95188f568d26c55bd20
pkprogramer61346134 <137704275+pkprogramer61346134@users.noreply.github.com>
2023-12-17 09:03:14 +0530

    updateuerders

--- Listofitems.js
+++ Listofitems.js
@@ -15,7 +15,21 @@ async function ItemslistCR(ITlist,ISlist)
      }
     
    for (let r = 0; r < Categorylist.length; r++) {
-      var newlist =  await elementCreator(Categorylist[r], "ul", 1, true, "slider0","ITList");
+      var listdiv  =  await elementCreator("div"+r, "div", 1, true, "slider0","ITList");
+      var newlist =  await elementCreator(Categorylist[r], "ul", 1, false, "div"+r,"ITList");
+      var liftbutton = await elementCreator("liftbutton", "button", 1, false, "div"+r,"liftbutton");
+      var rightbutton = await elementCreator("rightbutton", "button", 1, false, "div"+r,"rightbutton");
+      liftbutton.innerText = "<";
+      rightbutton.innerText = ">";
+      liftbutton.onclick = function(event) {
+        event.preventDefault();
+        scrollList(Categorylist[r], "left");
+    };
+    
+    rightbutton.onclick = function(event) {
+        event.preventDefault();
+        scrollList(Categorylist[r], "right");
+    };
     
       for (let y = 0; y < ITlist.message[0].message.Product.length; y++) {
 
@@ -24,19 +38,17 @@ async function ItemslistCR(ITlist,ISlist)
              if(Categorylist[r] == element.category)
              {
                       console.log(element);
-                         var liftbutton = await elementCreator("liftbutton", "button", 1, false, Categorylist[r],"liftbutton");
-                         var rightbutton = await elementCreator("rightbutton", "button", 1, false, Categorylist[r],"rightbutton");
-                         liftbutton.innerText = "<";
-                         rightbutton.innerText = ">";
+                       
+                       
                          var newli =  await elementCreator(element.Items_id, "li", 1, false, Categorylist[r],"lilist");
                          var imagestag =  await elementCreator("imagestag", "img", 1, false, element.Items_id,"imglist");
                          imagestag.src = element.Images[0];
                          var H1 = await elementCreator("heading", "h1", 1, false, element.Items_id,"h1list");
-                         H1.innerText = element.Name
+                         H1.innerText = element.Name;
                          var p =  await elementCreator("paragraph", "p", 1, false, element.Items_id,"plist");
-                         p.innerText = element.detail
+                         p.innerText = element.detail;
                          var h2 = await elementCreator("heading2", "h1", 1, false, element.Items_id,"h2list");
-                         h2.innerText = element.price
+                         h2.innerText = element.price;
                          
              }
       }
@@ -47,5 +59,23 @@ async function ItemslistCR(ITlist,ISlist)
    
     
       
+}
+
+function scrollList(ids,direction) {
+   var itemList = document.getElementById(ids);
+   var scrollAmount = 200; // Adjust the scroll amount as needed
+
+   if (direction === 'left') {
+       itemList.scrollLeft -= scrollAmount;
+   } else if (direction === 'right') {
+       itemList.scrollLeft += scrollAmount;
+   }
+
+   // Get the current scroll position or percentage scrolled
+   var scrollPosition = itemList.scrollLeft;
+   var totalWidth = itemList.scrollWidth - itemList.clientWidth;
+   var percentageScrolled = (scrollPosition / totalWidth) * 100;
+
+  
 }
 

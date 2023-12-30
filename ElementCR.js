d7e838b792607c50ac9d1b202bc23d2ca1275b64
pkprogramer61346134 <137704275+pkprogramer61346134@users.noreply.github.com>
2023-12-30 21:25:54 +0530

    sdfd

--- ElementCR.js
+++ ElementCR.js
@@ -1,15 +1,15 @@
-function elementCreator(name, element, count, place, elementIds, classname,) {
+function elementCreator(elementid, elementName,  elementPlace, PersionID, classname,) {
 
-        var newElement = document.createElement(element);
-        newElement.id = name;
+        var newElement = document.createElement(elementName);
+        newElement.id = elementid;
         newElement.classList.add(classname);
 
-        if (place) {
+        if (elementPlace) {
 
             document.body.appendChild(newElement);
             
         } else {
-            var parentElement = document.getElementById(elementIds);
+            var parentElement = document.getElementById(PersionID);
             parentElement.appendChild(newElement);
           
         }
